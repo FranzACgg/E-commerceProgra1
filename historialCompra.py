cdda2393b48a65718e6c58212567387ddf7aa126
Humberto Galindo <Hgalindo@uade.edu.ar>
2026-08-26 20:56:29 -0300

    agregado de funciones de calculo

--- historialCompra.py
+++ historialCompra.py
@@ -1,5 +1,13 @@
 from functools import reduce
 
-print("hello wold")
+def calcularPrecioProducto(producto):
+    """calcula el precio de un producto x=[id_producto,nombre,precio,stock]"""
+    return producto[2]*producto[3]
 
-#prueba
+def calcularTotalCarrito(carrito):
+    """calcula el total a pagar de la lista del carrito"""
+    if not carrito:
+        return 0.0
+    subtotales=list(map(lambda producto:producto[2]*producto[3],carrito))
+    total=reduce(lambda acumulador,subtotal:acumulador + subtotal, subtotales,0.0)
+    return round(total,2)
